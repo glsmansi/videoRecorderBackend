beae1447252a41cd51790e2fd6fc7e4451b117e8
mansi gubba <mansigubba@mansis-MacBook-Pro.local>
2022-03-23 22:58:58 +0530

    init commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const userRouter = require("./routes/user");
 
-const dbUrl = "mongodb://localhost:27017/extension";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/extension";
 
 mongoose
   .connect(dbUrl, {
