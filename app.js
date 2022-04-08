2406936f5870d2e593ffa10302b0679c52607d97
mansi gubba <mansigubba@mansis-MacBook-Pro.local>
2022-04-08 23:40:07 +0530

    init commit

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ dotenv.config();
 const cookieParser = require("cookie-parser");
 const user = require("./server/controllers/user");
 const methodOverride = require("method-override");
-const ejsMate = require("ejs-mate");
+const fileUpload = require("express-fileupload");
+//const expressLayouts=require('express-ejs-layouts')
 
 const userRouter = require("./server/routes/user");
 
 const sequelize = require("./server/database/connection");
-
 sequelize
   .authenticate()
   .then(() => console.log("Connected to Sequelize"))
@@ -22,10 +22,10 @@ sequelize
   });
 
 const app = express();
-
+app.use(fileUpload());
 app.set("view engine", "ejs");
+//app.use(expressLayouts);
 app.set("views", path.join(__dirname, "views"));
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
