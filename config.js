343527a87d109e9a0cd0de82cad7dc6aac00251b
mansi gubba <mansigubba@mansis-MacBook-Pro.local>
2022-04-24 23:40:03 +0530

    updated config file

--- config.js
+++ config.js
@@ -11,9 +11,11 @@ environments.production = {
   port: 8000,
   envName: "production",
 };
+console.log(process.env.NODE_ENV);
 
 var currentEnvironment =
   typeof process.env.NODE_ENV == "string" ? process.env.NODE_ENV : "";
+
 var environmentToExport =
   typeof environments[currentEnvironment] == "object"
     ? environments[currentEnvironment]
